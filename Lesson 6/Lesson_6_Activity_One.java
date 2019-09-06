6aeacaba27c0bc24166e64f8d86ca07e74ca1106
THOMAS <thomasbl830@scuc.txed.net>
2019-09-06 12:04:37 -0500

    09/06/2019 work update weekemd

---
+++ Lesson 6/Lesson_6_Activity_One.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Lesson_6_Activity_One {
+		public static void main (String [] args) {
+
+		int w,x,y,z;
+
+		Scanner scan = new Scanner (System.in);
+
+		System.out.println("Enter a three digit number: ");
+		z = scan.nextInt();
+		w = z % 7;
+		x = z % 11;
+		y = z % 21;
+		System.out.println(w + "\n" + x + "\n" + y);
+
+
+
+	}
+}
