45c9404e19b3261900d610d5b39de77842b85163
Blake <blakethomas0101@gmail.com>
2019-09-17 07:46:01 -0500

    commit 09/17/2019 weekend work update

--- APCOMPSCI/TimeConverter.java
+++ APCOMPSCI/TimeConverter.java
@@ -4,15 +4,21 @@ import java.lang.Math;
 public class TimeConverter {
 	public static void main (String[] args) {
 
-		int initSecs, years;
+		int initSecs, years, months, days, hours, mins, secs;
 		Scanner scan = new Scanner (System.in);
 
-		System.out.print("Please enter the desired seconds to convert: ");
+		System.out.print("Seconds since Unix Epoch: ");
 		initSecs = scan.nextInt();
 		System.out.print("\n");
 
-		years = (initSecs/31536000);
+		years = (1970 + (initSecs/31104000));
+		months = ((initSecs%31104000)/2592000);
+		days =   (((initSecs%31104000)%2592000)/86400);
+		hours = ((((initSecs%31104000)%2592000)%86400)/3600);
+		mins = (((((initSecs%31104000)%2592000)%86400)%3600)/60);
+		secs = (((((initSecs%31104000)%2592000)%86400)%3600)%60);
 
-		System.out.print(years);
+		System.out.print("Year: " + years + "\nMonth: " + months + "\nDay: " + days
+			+ "\nHour: " + hours + "\nMin: " + mins + "\nSec: " + secs);
 	}
 }
