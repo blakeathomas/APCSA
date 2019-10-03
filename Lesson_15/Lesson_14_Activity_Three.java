07cd2cabaf8dcd1a2603dfd447fba666e2362fff
Blake <blakethomas0101@gmail.com>
2019-10-03 11:33:33 -0500

    work update push 10/3/2019

--- Lesson_15/Lesson_14_Activity_Three.java
+++ Lesson_15/Lesson_14_Activity_Three.java
@@ -8,10 +8,11 @@ import java.lang.Math;
 public class Lesson_14_Activity_Three{
 
         public static void main (String str[]) throws IOException {
-        	int octOne, octTwo, octThree, octFour;
+        	int octOne, octTwo, octThree, octFour, one, two, three, four, error;
+        	String result = "";
         	Scanner scan = new Scanner (System.in);
 
-
+			error = 0;
         	System.out.println("Please enter the first octet:");
         	octOne = scan.nextInt();
 
@@ -24,8 +25,36 @@ public class Lesson_14_Activity_Three{
         	System.out.println("Please enter the fourth octet:");
         	octFour = scan.nextInt();
 
-			if (octOne, octTw
-        	System.out.println("IP Adress: " + octOne+ "." + octTwo + "." + octThree + "." + octFour);
+			if (octOne >= 0 && octOne <= 255){
+				result = (octOne + ".");
+			} else {
+				System.out.println("Octet 1 is incorrect");
+				error = error + 1;
+			}
+			if (octTwo >= 0 && octTwo <= 255){
+				result = (octOne + "." + octTwo + ".");
+			} else {
+				System.out.println("Octet 2 is incorrect");
+				error = error + 1;
+			}
+			if (octThree >= 0 && octThree <= 255){
+				result = (octOne + "." + octTwo + "." + octThree + ".");
+			} else {
+				System.out.println("Octet 3 is incorrect");
+				error = error + 1;
+			}
+			if (octFour >= 0 && octFour <= 255){
+				result = (octOne + "." + octTwo + "." + octThree + "." + octFour);
+			} else {
+				System.out.println("Octet 4 is incorrect");
+				error = error + 1;
+			}
+			if (error <= 0){
+				System.out.println("IP Address: " + result);
+			} else {
+				System.out.print("");
+			}
+
 
         }
 }
