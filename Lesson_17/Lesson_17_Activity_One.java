cab37c9a6b3e38f6b89d9f72da09f30ba4be408a
Blake Thomas <blakethomas0101@gmail.com>
2019-10-07 12:21:03 -0500

    folder changes 10/7/2019

---
+++ Lesson_17/Lesson_17_Activity_One.java
@@ -0,0 +1,30 @@
+/*
+ * Lesson 9 - Unit 1 - Math functions.
+ */
+import java.io.*;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+import java.lang.Math;
+
+public class Lesson_17_Activity_One{
+
+        public static void main (String str[]) throws IOException {
+     	Scanner scan = new Scanner (System.in);
+		double x = 0;
+		int y = 0;
+		double z = 0;
+     	while (x > -1){
+     	System.out.print("Please enter a grade: ");
+     	x = scan.nextDouble();
+     	z += x;
+     	y++;
+     	}
+		double average = (double)((z-1)/(y));
+		System.out.println("The average is: " + average);
+
+
+     }
+
+}
