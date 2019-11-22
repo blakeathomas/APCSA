dd6efe82682325fa64de31e0886ab5bb998d9b50
Blake Thomas <blakethomas0101@gmail.com>
2019-11-22 12:19:28 -0600

    work update 11/22/2019 final

--- APCOMPSCI/stringShortener.java
+++ APCOMPSCI/stringShortener.java
@@ -7,35 +7,38 @@ public class stringShortener {
 		int vowelCount = 0;
 		int repeatCount = 0;
 		int x = 0;
+		int len;
 		System.out.println("Type the message to be shortened: ");
 		String message = scan.nextLine();
+		message = message.toLowerCase();
+		len = message.length();
 
-		if ((message.length() < 10)){
+		if (len < 10){
 			System.out.println("This doesn't need shortening!");
 		} else {
-				while(x < message.length()){
+				while(x < len){
 					if	(((message.substring(x,x+1).equals("a"))) ||
 						((message.substring(x,x+1).equals("e"))) ||
 						((message.substring(x,x+1).equals("i"))) ||
 						((message.substring(x,x+1).equals("o"))) ||
 						((message.substring(x,x+1).equals("u")))){
-						vowelCount ++;
-						System.out.print(message.charAt(x) + "  " + x);
+						if (x == 0 || message.charAt(x-1) == ' ')
+							shortened += message.charAt(x);
+						else vowelCount ++;
 						x++;
-						} else if (message.charAt(x) == message.charAt(x+1)){
-							repeatCount ++;
-							System.out.print(message.charAt(x) + "  " + x);
-							x++;
-							} else	{
-								shortened += message.charAt(x);
-								System.out.print(message.charAt(x) + "  " + x);
-								x++;
+					} else if (x < (len-2) && message.charAt(x) == message.charAt(x+1)){
+						repeatCount ++;
+						x++;
+					} else{
+						shortened += message.charAt(x);
+						x++;
+					}
+				}
+				System.out.println("\nShortened message: " + shortened);
+				System.out.println("Repeated letters removed: " + repeatCount);
+				System.out.println("Vowels removed: " + vowelCount);
+				System.out.println("Total characters saved: " + (repeatCount + vowelCount));
+			}
 
-							  }
-						}
-		System.out.println(shortened);
-		System.out.println(x);
-		System.out.println(vowelCount);
 	}
-}
 }
