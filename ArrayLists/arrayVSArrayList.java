cf139b0a4628981b6c1dd9266283fb61cac1377c
Blake Thomas <blakethomas0101@gmail.com>
2020-01-10 12:12:33 -0600

    work update 1/10/2020 final

---
+++ ArrayLists/arrayVSArrayList.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class arrayVSArrayList{
+/*//9
+ name[3] = "Rattata";
+//10
+nameList.set(3, "Rattata");
+//11
+
+
+
+
+public static int searchArray (String [] arr, String search) {
+	for (int i=0; i < arr.length; i++) {
+		if (arr[i].compareTo(search) == 0)
+			return i;
+	}
+	return -1;
+}
+
+ public static void main(String args[]){
+
+    String [] array = {"dog", "cat", "mouse"};
+    int i =	searchArray(array, "mouse");
+ 	System.out.print(i);
+
+
+
+}
+}
+*/
+
+ArrayList <String> arrayList = new ArrayList <String>();
+arrayList.set
+
+}
