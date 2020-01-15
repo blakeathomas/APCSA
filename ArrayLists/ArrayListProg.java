14372ebc281c4ce0fbb9ace84831f2ca83d90245
Blake Thomas <blakethomas0101@gmail.com>
2020-01-15 12:19:59 -0600

    work update 1/15/2020 final

--- ArrayLists/ArrayListProg.java
+++ ArrayLists/ArrayListProg.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 
 public class ArrayListProg {
+	//Excersise 5
+	public static int countItems(ArrayList <String> list){	return list.size();	}
+	//Excersise 6
+	public static void removeLast(ArrayList&lt;String&gt; list),
 
     public static void main (String[] args) {
     	Scanner scan = new Scanner (System.in);
@@ -18,7 +22,7 @@ public class ArrayListProg {
 
     	System.out.println("You typed the following words: ");
 		for (String word : words){ System.out.println(word); }
-		*/
+
 		//Excersise 02:
 		ArrayList <String> words = new ArrayList <String> ();
     	boolean twice = false;
@@ -29,7 +33,49 @@ public class ArrayListProg {
     		else words.add(word);
     	}
 
+		//Excersise 03:
+		ArrayList <String> words = new ArrayList <String> ();
+    	boolean full = true;
+    	while (full){
+    		System.out.println("Type a word:");
+    		String word = scan.nextLine();
+    		if (word.isEmpty())	full = false;
+    		else words.add(word);
+    	}
+		Collections.reverse(words);
+    	System.out.println("You typed the following words: ");
+		for (String word : words){ System.out.println(word); }
+
+		//Excersise 04:
+		ArrayList <String> words = new ArrayList <String> ();
+    	boolean full = true;
+    	while (full){
+    		System.out.println("Type a word:");
+    		String word = scan.nextLine();
+    		if (word.isEmpty())	full = false;
+    		else words.add(word);
+    	}
+		Collections.reverse(words);
+    	System.out.println("You typed the following words: ");
+		for (String word : words){ System.out.println(word); }
+		*/
+
+		//Excersise 05: **See line 5 for method**
+		ArrayList <String> words = new ArrayList <String> ();
+		boolean full = true;
+    	while (full){
+    		System.out.println("Type a word:");
+    		String word = scan.nextLine();
+    		if (word.isEmpty())	full = false;
+    		else words.add(word);
+    	}
+
+    	System.out.print("There are this many items in the list: ");
+    	System.out.print(countItems(words));
+
     }
+
+
 
 
 }
