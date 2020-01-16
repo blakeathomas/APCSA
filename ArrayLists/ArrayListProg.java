0fc76ca504451604c900766188436b43afe21358
Blake Thomas <blakethomas0101@gmail.com>
2020-01-16 12:18:58 -0600

    work update 1/16/2020 final

--- ArrayLists/ArrayListProg.java
+++ ArrayLists/ArrayListProg.java
@@ -5,7 +5,16 @@ public class ArrayListProg {
 	//Excersise 5
 	public static int countItems(ArrayList <String> list){	return list.size();	}
 	//Excersise 6
-	public static void removeLast(ArrayList&lt;String&gt; list),
+	public static ArrayList<String> removeLast(ArrayList <String> list) {
+		list.remove(list.size() -1);
+		return list;
+	}
+	//Excersise 7
+	public static int sum(ArrayList <Integer> list) {
+		int sumList = 0;
+		for (Integer num : list){ sumList += num; }
+		return sumList;
+	}
 
     public static void main (String[] args) {
     	Scanner scan = new Scanner (System.in);
@@ -55,23 +64,48 @@ public class ArrayListProg {
     		if (word.isEmpty())	full = false;
     		else words.add(word);
     	}
-		Collections.reverse(words);
+		Collections.sort(words);
     	System.out.println("You typed the following words: ");
 		for (String word : words){ System.out.println(word); }
-		*/
 
 		//Excersise 05: **See line 5 for method**
 		ArrayList <String> words = new ArrayList <String> ();
 		boolean full = true;
     	while (full){
-    		System.out.println("Type a word:");
+    		System.out.println("Add a brother:");
     		String word = scan.nextLine();
     		if (word.isEmpty())	full = false;
     		else words.add(word);
     	}
-
     	System.out.print("There are this many items in the list: ");
-    	System.out.print(countItems(words));
+		System.out.print(countItems(words);
+
+
+		//Excersise 06: **See line 7 for method**
+		ArrayList <String> brothers = new ArrayList <String> ();
+		boolean full = true;
+    	while (full){
+    		System.out.println("Add a brother:");
+    		String brother = scan.nextLine();
+    		if (brother.isEmpty())	full = false;
+    		else brothers.add(brother);
+    	}
+		removeLast(brothers);
+    	System.out.print(brothers);
+
+
+    	//Excersise 07: **See line 12 for method**
+		ArrayList <Integer> list = new ArrayList <Integer>();
+		list.add(3);
+		list.add(2);
+		list.add(7);
+		list.add(2);
+
+		System.out.println("The sum: " + sum(list));
+		list.add(10);
+
+		System.out.println("the sum: " + sum(list));
+*/
 
     }
 
