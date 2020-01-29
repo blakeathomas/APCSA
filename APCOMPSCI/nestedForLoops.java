a59b2c9ad7c64513e869e8a38116fc68c6dbbaf7
Blake Thomas <blakethomas0101@gmail.com>
2020-01-28 20:06:28 -0600

    1/28/2019

--- APCOMPSCI/nestedForLoops.java
+++ APCOMPSCI/nestedForLoops.java
@@ -1,9 +1,42 @@
 public class nestedForLoops{
 	public static void main (String[] args){
+		System.out.println("1. ");
 		for (int i = 0; i<=5; i++){
-			for (int j=0; j<=5; j++){
-
+			for (int j = 0; j< i; j++){
+				System.out.print("*");
 			}
+			System.out.println();
+		}
+
+		System.out.println("2. ");
+		for (int i = 0; i<=5; i++){
+			for (int j = 5; j> i; j--){
+				System.out.print(" ");
+			}
+			for (int j = 0; j< i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("3. ");
+		for (int i = 1; i<=4; i++){
+			for (int j = 3; j>= i; j--){
+				System.out.print(" ");
+			}
+			for (int x = 1; x<=(2*i)-1; x++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("4. ");
+	//	public class getTable(int multiple;)
+		for (int i = 1; i< = 5; i++){
+			for (int j = 0; j>= 5; j++){
+				System.out.print(i*j);
+			}
+			System.out.println();
 		}
 	}
 }
